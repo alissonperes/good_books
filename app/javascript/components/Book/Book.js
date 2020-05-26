45441d3ed5bf874d0cfaed0d548a4806757f2f9c
Alisson Peres <aperes.dev@gmail.com>
2020-05-26 18:14:21 -0300

    created components for Books and Book

---
+++ app/javascript/components/Book/Book.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Book = () => {
+  return <div>Book show view</div>;
+};
+
+export default Book;
---
+++ app/javascript/components/Books/Books.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    // do something
+  });
+  return <div>Books index view</div>;
+};
+
+export default Books;
